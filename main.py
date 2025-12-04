0d62962dd7f6d54e262471b9066f6d742dc19f76
Iz-Alex <alexizotov88@rambler.ru>
2025-12-04 18:55:54 +0300

    update

--- main.py
+++ main.py
@@ -10,4 +10,4 @@ app.include_router(router_login)
 app.include_router(router_products)
 
 if __name__ == '__main__':
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=80, reload=True)
